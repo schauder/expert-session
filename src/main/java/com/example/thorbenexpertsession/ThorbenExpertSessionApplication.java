02e283e279ec2251142816ab4bf44cbdfadc9d27
Jens Schauder <jschauder@vmware.com>
2022-05-13 17:41:37 +0200

    Initial commit.

---
+++ src/main/java/com/example/thorbenexpertsession/ThorbenExpertSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.thorbenexpertsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThorbenExpertSessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThorbenExpertSessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thorbenexpertsession/ThorbenExpertSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thorbenexpertsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThorbenExpertSessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
